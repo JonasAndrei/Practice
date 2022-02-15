5fa87f20b3cf5cb1dabeb45126917f5c30fc7aca
Jonas Andrei <jonasandrei.ballarta@tup.edu.ph>
2022-02-15 15:53:46 +0800

    modify categoryPickerItem.js

--- app/components/CategoryPickerItem.js
+++ app/components/CategoryPickerItem.js
@@ -1,32 +1,34 @@
-import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import Icon from './Icon';
+import React from "react";
+import { StyleSheet, View, TouchableOpacity } from "react-native";
+
+import AppText from "./Text";
+import Icon from "./Icon";
 
 function CategoryPickerItem({ item, onPress }) {
-    return (  
-        <View style={styles.container}>
-            <TouchableOpacity onPress={onPress}>
-                    <Icon 
-                        backgroundColor={item.backgroundColor} 
-                        name={item.icon}
-                        size={80}
-                    />
-            </TouchableOpacity>
-            <Text style={styles.label}>{item.label}</Text>
-        </View>
-    )}
-
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={onPress}>
+        <Icon
+          backgroundColor={item.backgroundColor}
+          name={item.icon}
+          size={80}
+        />
+        <AppText style={styles.label}>{item.label}</AppText>
+      </TouchableOpacity>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-    container: {
-        paddingHorizontal: 30,
-        paddingVertical: 15,
-        alignItems: 'center',
-        width: '33%',
-    },
-    label: {
-        marginTop: 5,
-        textAlign: 'center',
-    }
-})
+  container: {
+    paddingHorizontal: 30,
+    paddingVertical: 15,
+    alignItems: "center",
+    width: "33%",
+  },
+  label: {
+    marginTop: 10,
+  },
+});
+
 export default CategoryPickerItem;
